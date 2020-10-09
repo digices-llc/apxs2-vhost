992579c6cf4af5959736c51af451eac474db6afe
Roderic Linguri <linguri@digices.com>
2020-10-09 15:21:58 -0600

    Draft setup script

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$packageName = "Example";
+$packageDescription = "An example index file";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php $packageName ?></title>
+</head>
+<body>
+  <h1><?php $packageName ?></h1>
+  <p><?php $packageDescription ?></p>
+</body>
+</html>
